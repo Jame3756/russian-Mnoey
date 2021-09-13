88085f1af28a380ef458ada654253b28e2a839c2
Jame3756 <75145664+Jame3756@users.noreply.github.com>
2021-09-12 17:15:06 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from "./screens/Home";
+import Calculator from "./screens/calculator";
+import Money from "./screens/moneytranslater";
+
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{
+        headerShown: false
+      }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Calculator" component={CalculatorScreen} />
+        <Stack.Screen name="Money" component={MoneyTranslatorScreen} />
+       
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
